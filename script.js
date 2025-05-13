e1a0c6f109009870fd5f3985233a8e9772066f1b
swayamrajora <127737419+swayamrajora@users.noreply.github.com>
2025-05-13 13:13:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 function validEmail(str) {
-  //your JS code here.
+  const emailRegex = /^[a-zA-Z0-9]+[._-]?[a-zA-Z0-9]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,3}$/;
+  return emailRegex.test(str);
 }
 
 // Do not change the code below.
